e6e3035ef0bdaca425c0a29ab28857a9bb219c08
irohanrajput <itusharsingh27@gmail.com>
2023-07-24 05:32:27 +0530

    created structure for a blog site

--- blog_project/blog_project/urls.py
+++ blog_project/blog_project/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('blog', include('blog.urls')),  # this inclusion of blog.urls is  responsible for bridge b/w our parent project and its sub-level apps. 
+    #when we'll go the something.com/blog, it will map that to "urls.py" file in "blog" APP that we created and then the empty in path in "urls.py" file in blog app will open the blog app
 ]
