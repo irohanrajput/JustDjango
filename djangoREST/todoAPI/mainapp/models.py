5baca3c4455a6ac88a6e33d61b45ed50e12382ca
Tushar Singh <itusharsingh27@gmail.com>
2023-11-11 17:21:28 +0000

    created models, serializers etc, about to wrap this up

---
+++ djangoREST/todoAPI/mainapp/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+class Todo(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    completed = models.BooleanField(default=False)
---
+++ djangoREST/todoAPI/mainapp/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import TodoViewSet
+
+router = DefaultRouter()
+router.register(r'todos', TodoViewSet)
+urlpatterns = [
+    path('', include(router.urls)),
+]
