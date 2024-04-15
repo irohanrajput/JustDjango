45808a36c6fb0a523ab305d38d8fabe68fbb8130
irohanrajput <itusharsingh27@gmail.com>
2024-04-15 14:04:01 +0530

    system backup

--- Archives/foreign_keys/myapp/models.py
+++ Archives/foreign_keys/myapp/models.py
@@ -10,7 +10,7 @@ class Author(models.Model):
 class Book(models.Model):
     title = models.CharField(("Title"), max_length=50)
     published = models.DateField(("Date Published"), auto_now=False, auto_now_add=False)
-    author = models.ForeignKey("Author", verbose_name=("Author"), on_delete=models.CASCADE)
+    author = models.ForeignKey("Author", verbose_name=("Author"), related_name='books', on_delete=models.CASCADE)
     
     def __str__(self) -> str:
         return self.title
--- authSystem/authapp/models.py
+++ authSystem/authapp/models.py
@@ -4,3 +4,4 @@ class UserProfile(models.Model):
     email = models.EmailField(unique=True)
     password = models.CharField(max_length=255)
     generated_otp = models.CharField(max_length=6, blank=True, null=True)
+    your = models.TextField(max_length = 4)
